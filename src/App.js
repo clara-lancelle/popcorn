d9416c4c449b5d0523d779e4499809b6e37aea72
pinklady <lancelle.clara>
2024-03-13 13:51:25 +0100

    first commit && add tailwind && router nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import FavPage from './Components/Dom/FavPage';
+import HomePage from './Components/Dom/HomePage';
+import Navbar from './Components/Dom/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/fav" element={<FavPage />} />
+      </Routes>
+    </div >
   );
 }
 
---
+++ src/Components/Dom/FavPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FavPage() {
+  return (
+    <div>
+      <h1>FavPage</h1>
+
+    </div>
+  );
+}
+
+export default FavPage;
